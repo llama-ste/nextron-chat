f5829549ef02deab1c9f22f536abe1ced0a310a1
llama-ste <andong1212@gmail.com>
2023-01-10 21:30:15 +0900

    chore: plugin (dotenv)추가

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,8 +1,12 @@
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.target = 'electron-renderer';
+      config.target = "electron-renderer";
     }
+
+    config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
   },
