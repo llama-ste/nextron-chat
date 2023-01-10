7a18ee63e1fd16f9b1f29531fe04ed3dd649f2a3
llama-ste <andong1212@gmail.com>
2023-01-10 21:44:07 +0900

    feat: app파일 초기셋팅 (lib provider 및 레이아웃 추가)

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,30 +1,87 @@
-import React from 'react';
-import Head from 'next/head';
-import type {AppProps} from 'next/app';
-import {CssBaseline, ThemeProvider} from '@mui/material';
-import theme from '../lib/theme';
-import type {EmotionCache} from "@emotion/cache";
-import createEmotionCache from '../lib/create-emotion-cache';
-import {CacheProvider} from '@emotion/react';
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import type { EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+
+import createEmotionCache from "../lib/create-emotion-cache";
+import theme from "../lib/theme";
+import Layout from "../components/Layout/Layout";
+import GlobalStyle from "../styles/GlobalStyle";
 
 const clientSideEmotionCache = createEmotionCache();
 
+const queryClientOptions = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  },
+};
+
+const toastConfig: object = {
+  autoClose: 500,
+  bodyStyle: { fontSize: "14px" },
+  closeButton: true,
+  closeOnClick: true,
+  limit: 1,
+  pauseOnHover: false,
+  position: "top-center",
+  progressStyle: {
+    background: `linear-gradient(
+      to right,
+      #4cd964,
+      #5ac8fa,
+      #007aff,
+      #34aadc,
+      #5856d6,
+      #ff2d55
+    )`,
+  },
+  style: {
+    maxWidth: "400px",
+    width: "max-content",
+    zIndex: 1000000,
+  },
+  theme: "light",
+};
+
 type MyAppProps = AppProps & {
-    emotionCache?: EmotionCache;
-}
+  emotionCache?: EmotionCache;
+};
 
-export default function MyApp(props: MyAppProps) {
-    const {Component, pageProps, emotionCache = clientSideEmotionCache} = props;
+export default function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  const queryClient = new QueryClient(queryClientOptions);
 
-    return (
-        <CacheProvider value={emotionCache}>
-            <Head>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </CacheProvider>
-    )
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <title>Nextron Chat</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer {...toastConfig} />
+          <CssBaseline />
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
